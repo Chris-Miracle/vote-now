993e8b1584cf83c1288b13eaa3c2c171ccd52a17
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-02-26 11:40:03 +0100

    Controller X Model

---
+++ database/migrations/2021_02_26_103506_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tally');
+            $table->string('email');
+            $table->string('contest');
+            $table->string('contestCode');
+            $table->string('ref');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
