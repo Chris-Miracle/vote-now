9c480ab2e32e821f53ea14e1af30493f73455cb2
Chris Miracle <55501479+Chris-Miracle@users.noreply.github.com>
2021-02-26 12:03:06 +0100

    Paystack integration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,14 +69,14 @@
         <div class="right-section">
             <h5>Purchase tally for John Doe</h5>
             <div class="close-btn"><i class="fa fa-close"></i></div>
-            <form action="">
+            <form action="" id="paynow">
                 <div class="form-group">
                     <label for="">How many tally votes?</label>
-                    <select class="form-control" name="tally" id="" placeholder="how many tally votes">
+                    <select class="form-control" name="tally" id="tally" placeholder="how many tally votes">
                         <option value=""></option>
-                        <option value="10">25</option>
-                        <option value="20">50</option>
-                        <option value="30">75</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="75">75</option>
                         <option value="100">100</option>
                     </select>
                 </div>
@@ -101,8 +101,8 @@
                     <p>By Clicking vote, you have agreed to user licence terms and conditions.</p>
                 </div>
 
-                <input type="hidden" name="refcode" id="refcode">
-                <input type="hidden" name="votes" id="votes" value="">
+                <input type="hidden" name="ref" id="ref">
+                <input type="hidden" name="amount" id="amount">
                 {{-- <div class="form-group">
                     <label for="refcode">Ref Code</label>
                     <input type="text" class="form-control" name="refcode" id="refcode">
@@ -114,7 +114,7 @@
                 </div> --}}
 
                 <div class="tally-btn-area" style="margin-top: 28px;">
-                    <button class="btn btn-primary vote-btn">Vote</button>
+                    <button type="button" onclick="paystackPay()" class="btn btn-primary vote-btn">Vote</button>
                 </div>
             </form>
         </div>
@@ -127,5 +127,39 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="{{asset('js/custom.js')}}"></script>
+    <script src="https://js.paystack.co/v1/inline.js"></script>
+    <script>
+        var tally = document.getElementById('tally');
+        tally.addEventListener('change', myFunction);
+
+        function myFunction(e){
+            e.preventDefault();
+            let amount = document.getElementById("tally").value * 50;
+            document.getElementById('amount').value = amount;
+        }
+
+        function paystackPay(){
+            var handler = PaystackPop.setup({
+                key: 'pk_test_8b5c0aeeac875fb820de39839ad121a93a82a055',
+                email: document.getElementById("email").value,
+                amount: document.getElementById("amount").value * 100,
+                currency: "NGN",
+                ref: 'BM-'+(Math.random().toString(36).substring(2, 16) + Math.random().toString(36).substring(2, 16)).toUpperCase(), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+            
+                callback: function(response){
+                    localStorage.removeItem("products");
+                    var reference = response.reference;
+                    document.getElementById('ref').value = reference;
+                    //alert('Payment complete! Reference: ' + reference);
+                    $('#paynow').submit();
+                },
+
+                onClose: function(){
+                    alert('Transaction was not completed, window closed.');
+                }
+            });
+            handler.openIframe();
+        }
+    </script>
 </body>
 </html>
